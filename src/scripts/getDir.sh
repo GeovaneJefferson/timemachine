abe437eb262b73b882a18ea5be5d555eb9b594c0
GeovaneJefferson <geovanejefferson@live.dk>
2022-03-03 17:37:41 +0100

    Bug fixes

---
+++ src/scripts/getDir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+check_file() {
+for f in $1:
+do
+    user_name=$(whoami)
+    f="$(basename -- $f)"
+    echo "$f" > /home/$user_name/.local/share/timemachine/src/scripts/restore_settings.txt
+    cd /home/$user_name/.local/share/timemachine/src/
+    python3 restore_screen.py
+done
+}
+check_file $PWD
