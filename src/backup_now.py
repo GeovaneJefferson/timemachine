d02567a1d6873082fb538fb7efc56f31156f930a
GeovaneJefferson <geovanejefferson@live.dk>
2022-03-17 15:48:36 +0100

    bug fixes

--- src/backup_now.py
+++ src/backup_now.py
@@ -7,83 +7,270 @@ config.read(src_user_config)
 
 class BACKUP:
     def __init__(self):
-        # Terminal commands
-        self.copyCmd = "rsync -avruzh "
-        self.createCmd = "mkdir "
+        ################################################################################
+        ## Commands
+        ################################################################################
+        self.copyCmd = "rsync -avruzh"
+        self.createCmd = "mkdir"
+        self.createFile = "touch"
 
         try:
-            config = configparser.ConfigParser()
-            config.read(src_user_config)
-            
-            # Get hour, minute
+            ################################################################################
+            ## Get hour, minute
+            ################################################################################
             dateTime = datetime.now()
             dateDay = dateTime.strftime("%d")
             dateMonth = dateTime.strftime("%m")
             dateYear = dateTime.strftime("%y")
             self.dayName = dateTime.strftime("%a")
-
             self.currentHour = dateTime.strftime("%H")
             self.currentMinute = dateTime.strftime("%M")
 
-            # Get user.ini
+            ################################################################################
+            ## Get user.ini
+            ################################################################################
             self.getExternalLocation = config['EXTERNAL']['hd']
             self.getHDName = config['EXTERNAL']['name']
             self.getIniFolders = config.options('FOLDER')
             self.backupNowChecker = config['BACKUP']['backup_now']
             self.oneTimeMode = config['MODE']['one_time_mode']
 
-            self.createTMB = self.getExternalLocation + "/TMB" 
-            self.dateFolder = f"{self.createTMB}/{dateDay}-{dateMonth}-{dateYear}" 
+            ################################################################################
+            ## Create folders
+            ################################################################################
+            self.createTMB = f"{self.getExternalLocation}/{folderName}"
+            self.dateFolder = f"{self.createTMB}/{dateDay}-{dateMonth}-{dateYear}"
             self.timeFolder = f"{self.createTMB}/{dateDay}-{dateMonth}-{dateYear}/{self.currentHour}-{self.currentMinute}"
 
-            self.create_TMB()   # If everything goes well, continue
-        
+            ################################################################################
+            ## Apt txt file
+            ################################################################################
+            self.apt_txt = f"{self.getExternalLocation}/{folderName}/apt.txt"
+
+            ################################################################################
+            ## Flatpak txt file
+            ################################################################################
+            self.flatpak_txt = f"{self.getExternalLocation}/{folderName}/flatpak.txt"
+
+            self.create_TMB()
+
         except KeyError:
             print("Error trying to read user.ini!")
             error_reading()
             exit()
 
     def create_TMB(self):
-        # Backup now True
+        ################################################################################
+        ## Create TMB
+        ################################################################################
         if self.backupNowChecker == "true":  # Read user.ini
             try:
-                # TMB folders
                 if os.path.exists(self.createTMB):
                     print("TMB folders inside external, already exist.")
                 else:
                     print("TMB folder inside external, was created.")
-                    sub.run(self.createCmd + self.createTMB, shell=True)    # Create tmb folder
+                    sub.run(f"{self.createCmd} {self.createTMB}", shell=True)
 
             except FileNotFoundError:
                 print("Error trying to create TMB...")
-                error_backup()  # Notification
+                error_backup()
                 exit()
         else:
             print("Nothing to do right now...")
             exit()
 
-        self.create_folder_date()  # Move on
+    #     self.create_apt_file()
+    #
+    # def create_apt_file(self):
+    #     ################################################################################
+    #     ## Create file txt
+    #     ################################################################################
+    #     try:
+    #         if os.path.exists(self.apt_txt):
+    #             print("Apt file already exist.")
+    #         else:
+    #             print("Apt file was created.")
+    #             sub.run(f"{self.createFile} {self.apt_txt}", shell=True)    # Create tmb folder
+    #
+    #     except FileNotFoundError:
+    #         print("Error trying to create Apt file...")
+    #         error_backup()  # Notification
+    #         exit()
+    #
+    #     self.create_flatpak_file()
+    #
+    # def create_flatpak_file(self):
+    #     ################################################################################
+    #     ## Create file txt
+    #     ################################################################################
+    #     try:
+    #         # TMB folders
+    #         if os.path.exists(self.flatpak_txt):
+    #             print("Flatpak file already exist.")
+    #         else:
+    #             print("Flatpak file was created.")
+    #             sub.run(f"{self.createFile} {self.flatpak_txt}", shell=True)    # Create tmb folder
+    #
+    #     except FileNotFoundError:
+    #         print("Error trying to create Flatpak file...")
+    #         error_backup()  # Notification
+    #         exit()
+    #
+    #     self.save_apt_file()
+    #
+    # def save_apt_file(self):
+    #     ################################################################################
+    #     ## Commands
+    #     ################################################################################
+    #     x = os.popen("apt-mark showmanual")
+    #
+    #     ################################################################################
+    #     ## Save to file
+    #     ################################################################################
+    #     list = []
+    #     for _ in x:
+    #         list.append(x.readline())
+    #
+    #     try:
+    #         with open(f"{self.getExternalLocation}/{folderName}/apt.txt", "w") as write_file:
+    #             for i in list:
+    #                 if not i.startswith(exclude):
+    #                     write_file.write(i)
+    #
+    #         ################################################################################
+    #         ## Read to file
+    #         ################################################################################
+    #         with open(f"{self.getExternalLocation}/{folderName}/apt.txt", "r") as read_file:
+    #             try:
+    #                 for i in read_file:
+    #                     print(i.strip())
+    #             except:
+    #                 pass  # Add do error list to install
+    #     except:
+    #         print("Error tying to save apt list of apps to the file!")
+    #         exit()
+    #
+    #     self.save_flatpak_file()
+    #
+    # def save_flatpak_file(self):
+    #     ################################################################################
+    #     ## Commands
+    #     ################################################################################
+    #     x = os.popen("flatpak list -a --columns=application --app")
+    #
+    #     ################################################################################
+    #     ## Save to file
+    #     ################################################################################
+    #     list = []
+    #     for _ in x:
+    #         list.append(x.readline())
+    #
+    #     try:
+    #         with open(f"{self.getExternalLocation}/{folderName}/flatpak.txt", "w") as write_file:
+    #             for i in list:
+    #                 if not i.startswith(exclude):
+    #                     write_file.write(i)
+    #
+    #         ################################################################################
+    #         ## Read to file
+    #         ################################################################################
+    #         with open(f"{self.getExternalLocation}/{folderName}/flatpak.txt", "r") as read_file:
+    #             try:
+    #                 for i in read_file:
+    #                     print(i.strip())
+    #             except:
+    #                 pass  # Add do error list to install
+    #     except:
+    #         print("Error trying to save flatpak list of apps to the file!")
+    #         exit()
+
+        self.check_size()
+
+    def check_size(self):
+        ################################################################################
+        ## Get current size
+        ################################################################################
+        used_space = os.popen(f"du -s {self.getExternalLocation}")
+        used_space = used_space.read().strip("\t").strip("\n").replace(self.getExternalLocation, "").replace("\t", "")
+        used_space = int(used_space)
+
+        ################################################################################
+        ## Get total size
+        ################################################################################
+        output_maximum = os.popen(f"df --output=size {self.getExternalLocation}")
+        output_maximum = output_maximum.read().strip().replace("1K-blocks", "").replace(" ", "")
+        output_maximum = int(output_maximum)
+
+        # Local variables
+        free_space = int(output_maximum - used_space)
+        limit = int(output_maximum * 0.8)   # 20% limit space
+
+        print(f"USB used space:   ", used_space)
+        print("USB maximum size:   ", output_maximum)
+        print("USB free size:   ", free_space)
+        print("USB limit space:   ", limit)
+
+        ################################################################################
+        ## Condition
+        ################################################################################
+        if used_space >= limit:
+            print("20% space limit reached!")
+            print("Old files will be deleted, to make room for the new ones.")
+            print("Please wait...")
+
+            ################################################################################
+            ## Get available dates inside TMB
+            ################################################################################
+            try:
+                dateFolders = []
+                for output in os.listdir(f"{self.getExternalLocation}/{folderName}"):
+                    dateFolders.append(output)
+                    dateFolders.sort(reverse=True, key=lambda date: datetime.strptime(date, "%d-%m-%y"))
+
+                print(dateFolders)
+                ################################################################################
+                ## Delete old folders
+                ################################################################################
+                sub.run(f"rm -rf {self.getExternalLocation}/{folderName}/{dateFolders[-1]}", shell=True)
+                print(f"Deleting {self.getExternalLocation}/{folderName}/{dateFolders[-1]}...")
+
+                self.create_folder_date()
+
+            except FileNotFoundError:
+                print("External not detected.")
+                not_available_notification()
+                exit()
+
+            print("Date available: ", self.dateFolders)
+        else:
+            self.create_folder_date()
 
     def create_folder_date(self):
+        ################################################################################
+        ## Create folder with date
+        ################################################################################
         try:
-            if os.path.exists(self.dateFolder): 
+            if os.path.exists(self.dateFolder):
                 pass
             else:
-                sub.run(self.createCmd + self.dateFolder, shell=True)   # Create folder with date
+                sub.run(f"{self.createCmd} {self.dateFolder}", shell=True)   # Create folder with date
 
         except FileNotFoundError:
             print("Error trying to create date folder...")
-            error_backup()  # Notification
+            error_backup()
             exit()
 
         self.create_folder_time()
 
     def create_folder_time(self):
+        ################################################################################
+        ## Create folder with time
+        ################################################################################
         try:
-            if os.path.exists(self.timeFolder): 
+            if os.path.exists(self.timeFolder):
                 pass
             else:
-                sub.run(self.createCmd + self.timeFolder, shell=True)   # Create folder with date
+                sub.run(f"{self.createCmd} {self.timeFolder}", shell=True)
 
         except FileNotFoundError:
             print("Error trying to create date folder...")
@@ -93,45 +280,50 @@ class BACKUP:
         self.start_backup()
 
     def start_backup(self):
+        ################################################################################
+        ## Start with the backup
+        ################################################################################
         try:
             if self.oneTimeMode == "true":
                 print("One mode activated!")
-                
+
                 for output in self.getIniFolders:    # Backup all (user.ini true folders)
                     output = output.title()  # Capitalize first letter. ex: '/Desktop'
                     print(f"Back up: {output}")
-                    sub.run(f"{self.copyCmd}{home_user}/{output} {self.timeFolder}", shell=True) #Ex: TMB/date/Desktop
+                    sub.run(f"{self.copyCmd} {home_user}/{output} {self.timeFolder}", shell=True) #Ex: TMB/date/Desktop
+
             else:
                 print("More mode activated!")
-                sub.run(self.createCmd + self.timeFolder, shell=True) # Create folder with date and time
-                
+                sub.run(f"{self.createCmd} {self.timeFolder}", shell=True) # Create folder with date and time
+
                 for output in self.getIniFolders:    # Backup all (user.ini true folders)
                     output = output.title()  # Capitalize first letter. ex: '/Desktop'
                     print(f"Back up: {output}")
-                    sub.run(f"{self.copyCmd}{home_user}/{output} {self.timeFolder}", shell=True) #Ex: TMB/date/time/Desktop
+                    sub.run(f"{self.copyCmd} {home_user}/{output} {self.timeFolder}", shell=True) #Ex: TMB/date/time/Desktop
                     print("")
 
         except FileNotFoundError:
             print("Error trying to back up folders to external location...")
-            error_backup()  # Notification
+            error_backup()
             exit()
 
         self.end_backup()
 
     def end_backup(self):
-        # Read ini file
-        config = configparser.ConfigParser()
-        config.read(src_user_config)
-
+        ################################################################################
+        ## Update "last backup" and set backup_now to "false"
+        ################################################################################
         with open(src_user_config, 'w') as configfile:
             config.set('BACKUP', 'backup_now', 'false')
             config.set('INFO', 'latest', f'{self.dayName}, {self.currentHour}:{self.currentMinute}')
             config.write(configfile)
 
-        # After backup is done
-        done_backup_notification()  # Notification
-        time.sleep(60)    
-        sub.Popen(f"python3 {src_backup_check_py}", shell=True)    # Call backup checker
+        ################################################################################
+        ## After backup is done
+        ################################################################################
+        done_backup_notification()
+        time.sleep(60) # Wait 60, so if it finish fast, wont repeat the backup :D
+        sub.Popen(f"python3 {src_backup_check_py}", shell=True)
         exit()
 
 
