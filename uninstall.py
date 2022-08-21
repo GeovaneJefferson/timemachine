9f1c96faea49cab0a52e2fe5a29d5c072db2629b
Geovane <geovanejefferson@live.dk>
2022-08-21 10:34:22 +0200

    fix bugs

--- uninstall.py
+++ uninstall.py
@@ -6,8 +6,8 @@ homeUser = str(Path.home())
 try:
     sub.run(f"rm -rf {homeUser}/.local/share/timemachine/", shell=True)
     sub.run(f"rm {homeUser}/.local/share/applications/timemachine.desktop", shell=True)
-    sub.run(f"rm {homeUser}/.config/autostart/backup_check.desktop", shell=True)
     sub.run(f"rm {homeUser}/.local/share/applications/migration_assistant.desktop", shell=True)
+    sub.run(f"rm {homeUser}/.config/autostart/backup_check.desktop", shell=True)
 
 except Exception as error:
     print(error)
