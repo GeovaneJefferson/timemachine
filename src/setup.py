56a4bdfb3aebe728fed8f8803c582419df8e9c06
Geovane <geovanejefferson@live.dk>
2022-11-11 10:24:12 +0100

    v1.1.3

--- src/setup.py
+++ src/setup.py
@@ -32,7 +32,7 @@ githubHome = "https://www.github.com/geovanejefferson/timemachine"
 # Names
 appName = "Time Machine"
 appNameClose = "timemachine"
-appVersion = "v1.1.4"
+appVersion = "v1.1.3"
 
 baseFolderName = "TMB"
 backupFolderName = "backups"
