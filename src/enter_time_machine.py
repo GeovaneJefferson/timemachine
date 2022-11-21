161c43b7b3afa294b387b9e3ae8d8e9187dd92f8
Geovane <geovanejefferson@live.dk>
2022-11-21 15:51:37 +0100

    Added dark mode

--- src/enter_time_machine.py
+++ src/enter_time_machine.py
@@ -797,6 +797,27 @@ class ENTERTIMEMACHINE(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(dark_palette)
+    app.setStyleSheet(
+        "QToolTip { color: #ffffff;"
+        "background-color: #2a82da;"
+        "border: 1px solid white; }")
+
     main = ENTERTIMEMACHINE()
     main.show()
     # Windows settings
