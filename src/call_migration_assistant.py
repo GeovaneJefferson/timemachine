0751787f6f21f9bc741fa5ec5a10c57007664d8c
Geovane <geovanejefferson@live.dk>
2023-01-02 10:18:48 +0100

    Updates

--- src/call_migration_assistant.py
+++ src/call_migration_assistant.py
@@ -4,8 +4,8 @@ if os.geteuid() == 0:
     print("All set!")
 else:
     print("Migration Assistant needs to run as root.")
-    sub.call(['sudo', 'python3', *sys.argv])
+    sub.run(['sudo', 'python3', *sys.argv])
     print("")
     print("Please wait...")
     print("")
-    sub.call(f"python3 {src_migration_assistant}", shell=True)
+    sub.run(f"python3 {src_migration_assistant}", shell=True)
