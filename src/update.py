18ec43a3d56413f4b7878535f0deec8f263aa629
Geovane <geovanejefferson@live.dk>
2023-02-04 13:19:56 +0100

    update

--- src/update.py
+++ src/update.py
@@ -2,25 +2,24 @@ from setup import *
 
 def update_app():
     # Check if ini file exist and can be found
-    if os.path.exists(src_user_config):
-        # Make a copy and move to /src
-        sub.run(
-            f"{copyCPCMD} {src_user_config} {homeUser}/.local/share/timemachine/src",shell=True)
+    # Make a copy and move to /src
+    print("Backup user.ini file")
+    sub.run(f"{copyCPCMD} {src_user_config} {homeUser}/.local/share/timemachine/src",shell=True)
 
     # Update
+    print("Updating...")
     os.popen("git stash; git pull")
 
-    # Delete the old ini file inside "ini" folder
-    # sub.run(f"rm {src_user_config}", shell=True)
-
     # Restore the copy to inside "ini" folder
-    # sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
-    sub.run(f"cp -rv {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
+    print("Deleting backup user.ini")
+    sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
 
     # Delete the copy
-    # sub.run(f"rm {homeUser}/.local/share/{appNameClose}/src/user.ini ", shell=True)
+    print("Deleting backup user.ini")
+    sub.run(f"rm {homeUser}/.local/share/{appNameClose}/src/user.ini ", shell=True)
 
     # Re-open app
+    print(f"Starting {src_main_window_py}")
     sub.Popen(f"python3 {src_main_window_py}", shell=True)
     exit()
 
