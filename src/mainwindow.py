f62ed6eb0bc1b009904f2c3a13943e102c3726d6
Geovane <geovanejefferson@live.dk>
2023-02-04 12:45:07 +0100

    Update

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -785,26 +785,6 @@ class MAIN(QMainWindow):
         ################################################################################
         sub.Popen(f"python3 {src_update_py}",shell=True)
         exit()
-        # # Check if ini file exist and can be found
-        # if os.path.exists(src_user_config):
-        #     # Make a copy a move to /src
-        #     sub.run(
-        #         f"{copyCPCMD} {src_user_config} {homeUser}/.local/share/timemachine/src",shell=True)
-        # # Update
-        # sub.run("git stash; git pull",shell=True)
-        # # Delete the old ini file inside "ini" folder
-        # sub.run(f"rm {src_user_config}", shell=True)
-        # # Restore the copy to inside "ini" folder
-        # sub.run(
-        #     f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
-        # # Delete the copy
-        # sub.run(f"rm {src_user_config}", shell=True)
-
-        # # Re-open app
-        # sub.Popen(f"python3 {src_main_window_py}", shell=True)
-        # # Exit the application to reload the new settings
-        # exit()
-
 
 class EXTERNAL(QWidget):
     def __init__(self):
