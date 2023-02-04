db21bde8425c8ee6d57d8aff66221c51fcb69d44
Geovane <geovanejefferson@live.dk>
2023-02-04 13:04:42 +0100

    update

--- src/update.py
+++ src/update.py
@@ -14,8 +14,7 @@ def update_app():
     # sub.run(f"rm {src_user_config}", shell=True)
 
     # Restore the copy to inside "ini" folder
-    print(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}")
-    sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
+    sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {homeUser}/.local/share/{appNameClose}/src/",shell=True)
 
     # Delete the copy
     # sub.run(f"rm {homeUser}/.local/share/{appNameClose}/src/user.ini ", shell=True)
