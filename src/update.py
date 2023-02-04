0e5d1b1a82731d27700fd70b72aadeac0db1ffca
Geovane <geovanejefferson@live.dk>
2023-02-04 13:00:47 +0100

    update

--- src/update.py
+++ src/update.py
@@ -3,18 +3,19 @@ from setup import *
 def update_app():
     # Check if ini file exist and can be found
     if os.path.exists(src_user_config):
-        # Make a copy a move to /src
+        # Make a copy and move to /src
         sub.run(
             f"{copyCPCMD} {src_user_config} {homeUser}/.local/share/timemachine/src",shell=True)
-
-    # Delete the old ini file inside "ini" folder
-    sub.run(f"rm {src_user_config}", shell=True)
 
     # Update
     os.popen("git stash; git pull")
 
+    # Delete the old ini file inside "ini" folder
+    # sub.run(f"rm {src_user_config}", shell=True)
+
     # Restore the copy to inside "ini" folder
-    sub.run(f"{copyRsyncCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {homeUser}/.local/share/timemachine/src/ini/",shell=True)
+    print(f"{copyRsyncCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}")
+    sub.run(f"{copyRsyncCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
 
     # Delete the copy
     # sub.run(f"rm {homeUser}/.local/share/{appNameClose}/src/user.ini ", shell=True)
