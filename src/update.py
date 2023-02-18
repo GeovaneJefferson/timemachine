ab61f95907661c0efbaa695a5e4e8f2d011830c5
Geovane <geovanejefferson@live.dk>
2023-02-18 12:27:33 +0100

    Updated

--- src/update.py
+++ src/update.py
@@ -3,25 +3,20 @@ from setup import *
 def backup_ini_file(updateNow):
     # Check if ini file exist and can be found
     # Make a copy and move to /src
-    print("Backup user.ini file")
     sub.run(f"{copyCPCMD} {src_user_config} {homeUser}/.local/share/timemachine/src",shell=True)
     if updateNow:
         update_git(updateNow)
 
 def update_git(updateNow):
-    print("Updating...")
     os.popen("git stash; git pull")
     if updateNow:
         restore_ini_file(updateNow)
 
 def delete_ini_file():
-    print("Deleting backup user.ini")
     sub.run(f"rm {homeUser}/.local/share/{appNameClose}/src/user.ini ", shell=True)
 
 def restore_ini_file(updateNow):
-    print("Restoring user.ini from backup location")
-    # sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
-    sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {homeUser}/Documents",shell=True)
+    sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {homeUser}/.local/share/{appNameClose}/src/ini/",shell=True)
     if updateNow:
         open_app(updateNow)
 
