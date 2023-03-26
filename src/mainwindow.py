7b7c892ec6a79fe572861110e06b19c9ca9219e4
Geovane <geovanejefferson@live.dk>
2023-03-26 14:08:23 +0200

    Updates

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -844,8 +844,9 @@ class MAIN(QMainWindow):
         # Updates found
         if "Your branch is behind" in str(gitUpdateCommand):
             updateAvailable = QPushButton()
-            updateAvailable.setText("  Update Available  ")
+            updateAvailable.setText("   Update Available   ")
             updateAvailable.adjustSize()
+            updateAvailable.setStyleSheet(buttonStylesheet)
             updateAvailable.clicked.connect(self.on_update_button_clicked)
             
             # Show button on screen      
