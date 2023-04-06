fdb32b77beba464372353f20716bc93df61ae1c7
Geovane <geovanejefferson@live.dk>
2023-04-06 14:03:14 +0200

    Updates

--- src/update.py
+++ src/update.py
@@ -7,25 +7,25 @@ def backup_ini_file(updateNow):
     """
     sub.run(f"{copyCPCMD} {src_user_config} {homeUser}/.local/share/{appNameClose}/src",shell=True)
     if updateNow:
-        delete_pycache_(updateNow)
+        delete_pycache_(True)
 
 def delete_pycache_(updateNow):
     print(f"Deleting {src_pycache}")
     sub.run(f"rm -rf {src_pycache}", shell=True)
     if updateNow:
-        update_git(updateNow)
+        update_git(True)
 
 def update_git(updateNow):
     print("Updating...")
     os.popen(f"cd /{homeUser}/.local/share/{appNameClose}/; git pull")
     if updateNow:
-        delete_ini_file(updateNow)
+        delete_ini_file(True)
 
 def delete_ini_file(updateNow):
     print("Deleting old ini file...")
     sub.run(f"rm -f {src_user_config}", shell=True)
     if updateNow:
-        restore_ini_file(updateNow)
+        restore_ini_file(True)
 
 def restore_ini_file(updateNow):
     sub.run(f"{copyCPCMD} {homeUser}/.local/share/{appNameClose}/src/user.ini {src_user_config}",shell=True)
@@ -35,4 +35,3 @@ def restore_ini_file(updateNow):
 def open_app():
     sub.Popen(f"python3 {src_main_window_py}", shell=True)
     exit()
-
