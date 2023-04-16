7cbd1e20faadf1d9e2bc3a5f63342e6b571bcaef
Geovane <geovanejefferson@live.dk>
2023-04-16 09:11:44 +0200

    Updates

--- src/systemtray.py
+++ src/systemtray.py
@@ -122,7 +122,7 @@ class APP:
                     self.backupNowButton.setEnabled(True)
                     self.browseTimeMachineBackupsButton.setEnabled(True)
 
-                    if calculate_time_left_to_backup() is not None:
+                    if calculate_time_left_to_backup() != None:
                         self.iniLastBackupInformation.setText(f'Next Backup to "{str(mainIniFile.ini_hd_name())}":')
                         self.iniLastBackupInformation2.setText(f'{str(calculate_time_left_to_backup())}\n')
                     else:
