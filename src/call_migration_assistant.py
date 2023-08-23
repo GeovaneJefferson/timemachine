16bd3247feb333fd8d435eed83b432c76faa1b89
Geovane <geovanejefferson@live.dk>
2023-08-23 21:08:14 +0200

    Updates

--- src/call_migration_assistant.py
+++ src/call_migration_assistant.py
@@ -1,12 +1,12 @@
 from setup import *
 
-# sub.run(['sudo', 'python3', *sys.argv])
 # Message
 print('Migration Assistant needs to run as root.')
 # Request users password
 sub.run(['sudo', 'python3', *sys.argv])
+# sub.run(['pkexec', 'python3', *sys.argv])
 # Clear terminal
 sub.run(['clear'])
 # Open Migration Assistant
-sub.Popen(["python3", SRC_MIGRATION_ASSISTANT_PY])
+sub.run(["python3", SRC_MIGRATION_ASSISTANT_PY])
  
