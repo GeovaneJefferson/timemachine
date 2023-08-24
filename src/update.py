97767a4db92a8f59f2a25fd2051f8e6743568697
Geovane <geovanejefferson@live.dk>
2023-08-24 14:08:17 +0200

    Updates

--- src/update.py
+++ src/update.py
@@ -2,9 +2,10 @@ from setup import *
 
 def backup_ini_file(update_now):
     # Make a copy of DB, and move it to src/
-    command = f"{SRC_USER_CONFIG_DB} {HOME_USER}/.local/share/{APP_NAME_CLOSE}/src"
-    sub.run(["cp", "-rv", command])
-
+    src = SRC_USER_CONFIG_DB 
+    dst = HOME_USER + "/.local/share/" + APP_NAME_CLOSE + "/src"
+    sub.run(["cp", "-rv", src, dst])
+            
     if update_now:
         command = src_pycache
         sub.run(["rm", "-rf", command])
@@ -46,4 +47,3 @@ def open_app():
     command = SRC_MAIN_WINDOW_PY
     sub.Popen(["python3", command])
     exit()
-    
