92c9ad510844072cf0c8c5bac4078f6e0f9437bd
Geovane <geovanejefferson@live.dk>
2023-10-01 11:03:29 +0200

    Test

--- src/setup.py
+++ src/setup.py
@@ -244,19 +244,19 @@ LOG_LOCATION = f'{HOME_USER}/Documents/.log_time_machine.txt'
 from read_ini_file import UPDATEINIFILE
 MAIN_INI_FILE = UPDATEINIFILE()
 
-def signal_exit(*args):
+def signal_exit(message):
     # Set unafineshed backup to Yes
     # MAIN_INI_FILE.set_database_value('STATUS', 'unfinished_backup', 'Yes')
 
-    # # Check if location exist
-    # if not os.path.exists(LOG_LOCATION):
-    #     # Create file
-    #     sub.run(['touch', LOG_LOCATION],
-    #         stdout=sub.PIPE,
-    #         stderr=sub.PIPE)
+    # Check if location exist
+    if not os.path.exists(LOG_LOCATION):
+        # Create file
+        sub.run(['touch', LOG_LOCATION],
+            stdout=sub.PIPE,
+            stderr=sub.PIPE)
         
-    # with open(LOG_LOCATION, 'w') as writer:
-    #     writer.write(*args)
+    with open(LOG_LOCATION, 'w') as writer:
+        writer.write(message)
         
     exit()
 
