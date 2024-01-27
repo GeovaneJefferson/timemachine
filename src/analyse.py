4b620826f52c4ca882721b10a8e19015024d0fae
Geovane <geovanejefferson@live.dk>
2024-01-27 12:22:20 +0100

    Updates

--- src/analyse.py
+++ src/analyse.py
@@ -655,37 +655,37 @@ def need_to_backup_analyse():
     return True 
     
 
-if __name__ == '__main__':
-    # Update notification
-    print('Analysing backup...')
+# if __name__ == '__main__':
+# Update notification
+print('Analysing backup...')
 
-    notification_message('Analysing backup...')
+notification_message('Analysing backup...')
 
-    # # Set backing up now to True
-    # MAIN_INI_FILE.set_database_value(
-    #     'STATUS', 'backing_up_now', 'True') 
+# # Set backing up now to True
+# MAIN_INI_FILE.set_database_value(
+#     'STATUS', 'backing_up_now', 'True') 
 
-    # need_to_backup_analyse()
+# need_to_backup_analyse()
 
-    # Need to backup
-    if need_to_backup_analyse():
-        # Prepare backup
-        if MAIN_PREPARE.prepare_the_backup():
-            print('Calling backup now...')
+# Need to backup
+if need_to_backup_analyse():
+    # Prepare backup
+    if MAIN_PREPARE.prepare_the_backup():
+        print('Calling backup now...')
 
-            # Backup now
-            sub.Popen(
-                ['python3', SRC_BACKUP_NOW_PY], 
-                    stdout=sub.PIPE, 
-                    stderr=sub.PIPE)  
-
-    else:
-        # Backing up to False
-        MAIN_INI_FILE.set_database_value(
-            'STATUS', 'backing_up_now', 'False')   
-
-        # Re.open backup checker
+        # Backup now
         sub.Popen(
-            ['python3', SRC_BACKUP_CHECKER_PY], 
-            stdout=sub.PIPE, 
-            stderr=sub.PIPE)
+            ['python3', SRC_BACKUP_NOW_PY], 
+                stdout=sub.PIPE, 
+                stderr=sub.PIPE)  
+
+else:
+    # Backing up to False
+    MAIN_INI_FILE.set_database_value(
+        'STATUS', 'backing_up_now', 'False')   
+
+    # Re.open backup checker
+    sub.Popen(
+        ['python3', SRC_BACKUP_CHECKER_PY], 
+        stdout=sub.PIPE, 
+        stderr=sub.PIPE)
